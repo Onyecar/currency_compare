a641615629be77a3ef3df99ce51ef392342b093c
Onyeka <onyeka.anene@gmail.com>
2017-11-04 00:52:57 +0100

    first commit

---
+++ app/src/main/java/com/onyx/cryptocompare/ExchangeAdapter.java
@@ -0,0 +1,8 @@
+package com.onyx.cryptocompare;
+
+/**
+ * Created by onyekaanene on 04/11/2017.
+ */
+
+public class ExchangeAdapter {
+}
---
+++ app/src/main/java/com/onyx/cryptocompare/MainActivity.java
@@ -0,0 +1,30 @@
+package com.onyx.cryptocompare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_exchange);
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+    }
+}
